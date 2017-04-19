fe5820371fa241ad177d8a3ae65c7dbb053bcfed
Pandarizer <dmytro.bukanov@gmail.com>
2017-04-19 17:48:56 +0200

    fast fix

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println(" |Enter expression. Example: a + b; a * b|");
-        System.out.println("***Remember not put space between operators and operands!***")
+        System.out.println("***Remember not put space between operators and operands!***");
         Pattern pat = Pattern.compile(" ");
         Scanner in = new Scanner(System.in);
         while (true) {
