350b96f56ebfb494dd3ae9a4ae67ef1659c054c4
pandarizer <dmytro.bukanov@gmail.com>
2017-05-23 22:01:20 +0200

    samples of boxing and unboxing

--- src/Basic_stuff.java
+++ src/Basic_stuff.java
@@ -1,7 +1,23 @@
-package PACKAGE_NAME;
-
 /**
  * Created by dbuka on 23.05.2017.
  */
 public class Basic_stuff {
+
+    //ПРИМЕР №1
+    //автоупаковка и распаковка с оператором сравнения
+    //
+    public static void box_unbox()
+    {
+        Integer in = new Integer(25);
+        if (in < 35)
+            System.out.println("Value of int = " + in);
+    }
+
+    public static void main(String[] args)
+    {
+        box_unbox();
+
+    }
 }
+
+
